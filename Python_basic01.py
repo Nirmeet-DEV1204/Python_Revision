030a39f4e337bbd45b254a1aee10e9d2a8434f48
Nirmeet-Dev1204 <dev.nirmeet07@gmail.com>
2023-09-27 22:50:05 -0400

    First day of revison

---
+++ Python_basic01.py
@@ -0,0 +1,24 @@
+#Author:Nirmeet_Pansuriya
+#Date:27 September 2023
+#########################################################################################################################################################################
+# Day:1
+
+# Python is a object orianted, high level orianted language. 
+# Use to create web application, handle big data and can performance high complex math problems 
+
+# Starting from "Varabials" it can be: letter or the under_score.
+#                                      cannot start with a number.
+#                                      They are case-Sensitive
+#                                      They Cannot be python keyword
+#                                      Eg.(A-a;_;0-9) 
+
+# Camel Case:- myNameVariable:(First letter small and rest all are Capital)
+# Pascal Case:-MyNameVariable:(All First letter are Capital)
+# Snake Case:- my_name_variable:(All letter are small and are seprated by under_score)
+
+# Assign Multipal Value:  X=Y=Z="orange"(many variables one value)
+#                             or
+#                         X=Y=Z="orange","banana","apple"(many valur to many variables)
+#                             or 
+#                         fruit=["apple","banana","orange"](Unpacking a collection)
+#                         x,y,z=fruit
